615449b58a7dac365ca8c65db2be29afb140b5ef
Pooria Azami <pooriaazami@gmail.com>
2022-02-17 14:22:45 +0330

    optimizaition

--- MatrixImage.py
+++ MatrixImage.py
@@ -14,11 +14,6 @@ class MatrixImage:
     def shape(self):
         return self.__width // self.__density, self.__height // self.__density
 
-    def __fill(self, x_start, x_end, y_start, y_end, color):
-        for j in range(x_start, x_end):
-            for i in range(y_start, y_end):
-                self.__image[i, j] = color
-
     def fill_rectangle(self, x, y, color):
         x_start = x * self.__density
         x_end = x_start + self.__density
@@ -26,7 +21,7 @@ class MatrixImage:
         y_start = y * self.__density
         y_end = y_start + self.__density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     def fill_upper_edge(self, x, y, color):
         x_start = x * self.__density
@@ -35,7 +30,7 @@ class MatrixImage:
         y_start = y * self.__density
         y_end = y_start + self.__edge_density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     def fill_lower_edge(self, x, y, color):
         x_start = x * self.__density
@@ -44,7 +39,7 @@ class MatrixImage:
         y_start = (y + 1) * self.__density - self.__edge_density
         y_end = y_start + self.__edge_density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     def fill_left_edge(self, x, y, color):
         x_start = x * self.__density
@@ -53,7 +48,7 @@ class MatrixImage:
         y_start = y * self.__density
         y_end = y_start + self.__density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     def fill_right_edge(self, x, y, color):
         x_start = (x + 1) * self.__density - self.__edge_density
@@ -62,7 +57,7 @@ class MatrixImage:
         y_start = y * self.__density
         y_end = y_start + self.__density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     def fill_center(self, x, y, color):
         x_start = x * self.__density + self.__edge_density
@@ -71,7 +66,7 @@ class MatrixImage:
         y_start = y * self.__density + self.__edge_density
         y_end = y_start + self.__density - 2 * self.__edge_density
 
-        self.__fill(x_start, x_end, y_start, y_end, color)
+        self.__image[y_start:y_end, x_start:x_end] = color
 
     @property
     def image(self):
